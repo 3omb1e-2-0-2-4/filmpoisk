767f1d9c0cd8d27ccc6066a0b347e5ce3614aa44
Sasha Kuznetsov <hakersasha01@gmail.com>
2024-08-18 00:01:09 +0300

    filmpoisk
    filmpoisk

---
+++ index.js
@@ -0,0 +1,9 @@
+const input = document.querySelector("#search");
+const divs = document.querySelectorAll(".movie");
+
+input.addEventListener("keyup", function(event) {
+    const word = event.target.value.toLowerCase();
+    divs.forEach(item => {
+        item.querySelector("p").textContent.toLowerCase().includes(word) ? (item.style.display = "block") : (item.style.display="none");
+    })
+})
